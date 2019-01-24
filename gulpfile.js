21901a86b2654790f42e795d28070766f2ef3fde
Aydin <ahepsaydir@and.digital>
2019-01-24 14:12:34 +0000

    added sass watcher functionality and html and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,34 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
 
-gulp.task("hello", function() {
-  console.log("hello aydin");
+gulp.task("sass", function() {
+  return gulp
+    .src("app/style/scss/stylesheet.scss") // Get source files with gulp.src
+    .pipe(sass()) // Sends it through a gulp plugin
+    .pipe(gulp.dest("app/style/css")) // Outputs the file in the destination folder
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
+
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+});
+
+gulp.task("watch", ["browserSync", 'sass'], function() {
+
+  //reload whenever sass file is changed and saves
+  gulp.watch("./app/scss/**/*.scss", ["sass"]);
+
+  //reload whenever js or html is changed and saved 
+  gulp.watch('./app/*.html', borwserSync.reload);
+  gulp.watch('./app/js/**/*.js', browserSync.reload);
+  
 });
